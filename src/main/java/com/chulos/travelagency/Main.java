812a36dd6912fd9561243967b154daf43b7b9cfa
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-19 09:52:13 -0500

    docs(REMOVIDO): :fire: Se removio codigo de la clase Main
    Se removio el test hecho anteriormente para que los demas intergrantes del equipo puedan hacer los correspondientes tests

--- src/main/java/com/chulos/travelagency/Main.java
+++ src/main/java/com/chulos/travelagency/Main.java
@@ -1,13 +1,7 @@
 package com.chulos.travelagency;
 
-import java.util.Scanner;
-
-import com.chulos.travelagency.trip.infrastructure.in.TripController;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        TripController tripController = new TripController(scanner);
-        tripController.run();
+        
     }
 }
