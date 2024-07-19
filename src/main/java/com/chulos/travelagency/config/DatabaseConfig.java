7fc3d81d1e1a07d005c924c475714726eda3fbf2
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-19 01:15:58 -0500

    docs(Avanzando): :construction: se hicieron los procedimientos
    Se terminaron los procedimientos en la base de datos de tripbooking
    
    Aun pueden haber modificaciones por hacer

--- src/main/java/com/chulos/travelagency/config/DatabaseConfig.java
+++ src/main/java/com/chulos/travelagency/config/DatabaseConfig.java
@@ -1,17 +1,17 @@
-    package com.chulos.travelagency.config;
+package com.chulos.travelagency.config;
 
-    import java.sql.Connection;
-    import java.sql.DriverManager;
-    import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    public class DatabaseConfig {
-        // attributes
-        private static final String URL = "jdbc:mysql://monorail.proxy.rlwy.net:41941/TravelAgency";
-        private static final String USER = "root";
-        private static final String PASSWORD = "OJXhzGFHvvbZhUbgIcEpbptVOBBAOMaE";
+public class DatabaseConfig {
+    // attributes
+    private static final String URL = "jdbc:mysql://monorail.proxy.rlwy.net:41941/TravelAgency";
+    private static final String USER = "root";
+    private static final String PASSWORD = "OJXhzGFHvvbZhUbgIcEpbptVOBBAOMaE";
 
-        // method to get connection
-        public static Connection getConnection() throws SQLException {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        }
+    // method to get connection
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+}
