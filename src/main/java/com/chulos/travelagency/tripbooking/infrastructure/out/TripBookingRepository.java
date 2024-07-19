8fe2c90c0cef135312def95af7bced6a4e4e6ab1
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-19 01:15:58 -0500

    docs(Avanzando): :construction: se hicieron los procedimientos
    Se terminaron los procedimientos en la base de datos de tripbooking
    
    Aun pueden haber modificaciones por hacer

--- src/main/java/com/chulos/travelagency/tripbooking/infrastructure/out/TripBookingRepository.java
+++ src/main/java/com/chulos/travelagency/tripbooking/infrastructure/out/TripBookingRepository.java
@@ -1,5 +1,33 @@
 package com.chulos.travelagency.tripbooking.infrastructure.out;
 
-public class TripBookingRepository {
+import com.chulos.travelagency.tripbooking.domain.entity.TripBooking;
+import com.chulos.travelagency.tripbooking.domain.service.TripBookingService;
+
+public class TripBookingRepository implements TripBookingService{
+
+    @Override
+    public String addTripReservation(TripBooking tripBooking) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTripReservation'");
+    }
+
+    @Override
+    public String deleteTripBooking(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTripBooking'");
+    }
+
+    @Override
+    public String updateTripBooking(TripBooking tripBooking) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTripBooking'");
+    }
+
+    @Override
+    public TripBooking findTripBooking(int id_customer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findTripBooking'");
+    }
 
 }
+    
