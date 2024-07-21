023473a01aca6c686afe43470b5010c58bf8c33b
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-20 21:13:27 -0500

    build(MERGEADO): :rewind: Se fusionaron las ramas trip_booking y developer
    Se agrego la nueva entidad trip_booking
    
    Todo testeado correctamente

--- src/main/java/com/chulos/travelagency/Main.java
+++ src/main/java/com/chulos/travelagency/Main.java
@@ -1,20 +1,12 @@
 package com.chulos.travelagency;
 
-<<<<<<< HEAD
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-=======
 import java.util.Scanner;
 
 import com.chulos.travelagency.tripbooking.infrastructure.in.TripBookingController;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TripBookingController tripBookingController = new TripBookingController(scanner);
         tripBookingController.run();
     }   
->>>>>>> trip_booking
 }
