4d9b879783a3dfed86d8a1675c6f0bf1d98540b4
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-21 21:30:32 -0500

    test: :white_check_mark: Se finalizo la entidad payment
    Se finalizo la entidad payment, todo esta correctamente testeado
    
    Ningun error por verificar

--- src/main/java/com/chulos/travelagency/Main.java
+++ src/main/java/com/chulos/travelagency/Main.java
@@ -6,8 +6,5 @@ import com.chulos.travelagency.auth.infrastructure.in.AuthController;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        AuthController authController = new AuthController(scanner);
-        authController.start();
-    }
+    }   
 }
