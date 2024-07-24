1bd3a3ff9c41ade5e7f1dd3cf1bd13ed987a4062
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-24 14:35:44 -0500

    modificados:     src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolSalesAgentController.java 	modificados:     target/classes/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolSalesAgentController.class

--- src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolSalesAgentController.java
+++ src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolSalesAgentController.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import com.chulos.travelagency.auth.domain.service.IMenuController;
 import com.chulos.travelagency.plane.infrastructure.in.PlaneController;
+import com.chulos.travelagency.tripbooking.infrastructure.in.TripBookingController;
+import com.chulos.travelagency.utils.MyUtils;
 
 public class RolSalesAgentController implements IMenuController{
     private final Scanner scanner;
@@ -19,59 +21,23 @@ public class RolSalesAgentController implements IMenuController{
             System.out.println("\n╔═══════════════════════════════════════════╗");
             System.out.println(  "║              MENU SALES AGENT             ║");
             System.out.println(  "├═══════════════════════════════════════════┤");
-            System.out.println(  "║ 1. Crear Reserva de Vuelo                 ║");
-            System.out.println(  "║ 2. Consultar Información de Cliente       ║");
-            System.out.println(  "║ 3. Consultar Reserva de Vuelo             ║");
-            System.out.println(  "║ 4. Registrar Cliente                      ║");
-            System.out.println(  "║ 5. Actualizar Información de CLiente      ║");
-            System.out.println(  "║ 6. Eliminar Reserva de Vuelo              ║");
-            System.out.println(  "║ 7. Consultar Información de Vuelo         ║");
-            System.out.println(  "║ 8. Consultar Asignación de Tripulación    ║");
-            System.out.println(  "║ 9. Consultar Escalas de un Trayecto       ║");
-            System.out.println(  "║ 10. Consultar Tarifa de Vuelo             ║");
-            System.out.println(  "║ 11. Consultar Tipo de Documento           ║");
-            System.out.println(  "║ 12. Salir                                 ║");
+            System.out.println(  "║ 1. Reservas de vuelo                      ║");
+            System.out.println(  "║ 2. Seccion Clientes                       ║");
+            System.out.println(  "║ 3. Salir                                 ║");
             System.out.println(  "╚═══════════════════════════════════════════╝");
             System.out.print("Seleccione una opción: ");
             option = scanner.nextInt();
 
             switch (option) {
                 case 1:
-                    PlaneController planecontroller = new PlaneController(scanner);
-                    planecontroller.run();
-                    
+                    TripBookingController tripBookingController = new TripBookingController(scanner);
+                    tripBookingController.run();     
+                    break;       
                 case 2:
                     System.out.println(" - Lógica no implementada aún");
                     break;
                 case 3:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 4:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 5:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 6:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 7:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 8:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 9:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 10:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 11:
-                    System.out.println(" - Lógica no implementada aún");
-                    break;
-                case 12:
-                    System.out.println("Saliendo...");
+                    MyUtils.displayMessageAndClearScreen("Exiting", 2);
                     break;
                 default:
                     System.out.println("Opción no válida. Intente nuevamente.");
