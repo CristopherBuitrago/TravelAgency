4a5b9c5ba59c464dfccc764755dc5590865010d9
danielnavasbecerra <danielnavasb14@gmail.com>
2024-07-24 14:50:59 -0500

    modificados:     src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolAdminController.java 	modificados:     target/classes/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolAdminController.class

--- src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolAdminController.java
+++ src/main/java/com/chulos/travelagency/auth/infrastructure/in/rol_controller/RolAdminController.java
@@ -1,5 +1,7 @@
 package com.chulos.travelagency.auth.infrastructure.in.rol_controller;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import com.chulos.travelagency.auth.domain.service.IMenuController;
@@ -8,6 +10,7 @@ import com.chulos.travelagency.flight.infrastructure.in.FlightController;
 import com.chulos.travelagency.plane.infrastructure.in.PlaneController;
 import com.chulos.travelagency.trip.infrastructure.in.TripController;
 import com.chulos.travelagency.tripcrew.infrastructure.in.CrewController;
+import com.chulos.travelagency.utils.MyUtils;
 
 public class RolAdminController implements IMenuController {
     private final Scanner scanner;
@@ -18,48 +21,61 @@ public class RolAdminController implements IMenuController {
 
     @Override
     public void showMenu() {
-        int option;
+        int option = 0;
         do {
-            System.out.println("\n╔═══════════════════════════════════════════╗");
-            System.out.println("║             MENU ADMINISTRATOR            ║");
-            System.out.println("├═══════════════════════════════════════════┤");
-            System.out.println("║ 1. Registrar Avión                        ║");
-            System.out.println("║ 2. Asignar Tripulación a Trayecto         ║");
-            System.out.println("║ 3. Crear vuelo                            ║");
-            System.out.println("║ 4. Sección Trayectos                      ║");
-            System.out.println("║ 5. Sección Clientes                       ║");
-            System.out.println("║ 6. Salir                                  ║");
-            System.out.println("╚═══════════════════════════════════════════╝");
-            System.out.print("Seleccione una opción: ");
-            option = scanner.nextInt();
+            try {
+                System.out.println("\n╔═══════════════════════════════════════════╗");
+                System.out.println("║             MENU ADMINISTRATOR            ║");
+                System.out.println("├═══════════════════════════════════════════┤");
+                System.out.println("║ 1. Registrar Avión                        ║");
+                System.out.println("║ 2. Asignar Tripulación a Trayecto         ║");
+                System.out.println("║ 3. Crear vuelo                            ║");
+                System.out.println("║ 4. Sección Trayectos                      ║");
+                System.out.println("║ 5. Sección Clientes                       ║");
+                System.out.println("║ 6. Salir                                  ║");
+                System.out.println("╚═══════════════════════════════════════════╝");
+                System.out.print("Seleccione una opción: ");
+                option = scanner.nextInt();
 
-            switch (option) {
-                case 1:
-                    PlaneController planecontroller = new PlaneController(scanner);
-                    planecontroller.run();
-                    break;
-                case 2:
-                    CrewController crewController = new CrewController(scanner);
-                    crewController.run();
-                    break;
-                case 3:
-                    FlightController flightController = new FlightController(scanner);
-                    flightController.run();
-                    break;
-                case 4:
-                    TripController tripController = new TripController(scanner);
-                    tripController.run();
-                    break;
-                case 5:
-                    CustomerController customerController = new CustomerController(scanner);
-                    customerController.run();
-                    break;
-                case 6:
-                    System.out.println("Saliendo...");
-                    break;
-                default:
-                    System.out.println("Opción no válida. Intente nuevamente.");
+                switch (option) {
+                    case 1:
+                        PlaneController planecontroller = new PlaneController(scanner);
+                        planecontroller.run();
+                        break;
+                    case 2:
+                        CrewController crewController = new CrewController(scanner);
+                        crewController.run();
+                        break;
+                    case 3:
+                        FlightController flightController = new FlightController(scanner);
+                        flightController.run();
+                        break;
+                    case 4:
+                        TripController tripController = new TripController(scanner);
+                        tripController.run();
+                        break;
+                    case 5:
+                        CustomerController customerController = new CustomerController(scanner);
+                        customerController.run();
+                        break;
+                    case 6:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Intente nuevamente.");
+                }
+
+            } catch (InputMismatchException e) {
+                scanner.nextLine();
+                MyUtils.displayMessageAndClearScreen("Only numbers are valid", 2);
+            } catch (IllegalStateException e) {
+                System.out.println("ERROR: Scanner not exists.");
+                return;
+            } catch (NoSuchElementException e) {
+                System.out.println("Error: No more input available");
+                return;
             }
-        } while (option != 5);
+
+        } while (option != 6);
     }
 }
