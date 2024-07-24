5014d60189d685081f8b2450042233d217cf90f3
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-07-24 09:26:23 -0500

    refactor(REMOVIDO): :fire: se removio codigo de la clase main

--- src/main/java/com/chulos/travelagency/Main.java
+++ src/main/java/com/chulos/travelagency/Main.java
@@ -1,18 +1,7 @@
 package com.chulos.travelagency;
 
-import java.util.Scanner;
-
-import com.chulos.travelagency.auth.infrastructure.in.AuthController;
-import com.chulos.travelagency.flight.infrastructure.in.FlightController;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // AuthController authController = new AuthController(scanner);
-        // authController.start();
-
-        // test flight
-        FlightController flightController = new FlightController(scanner);
-        flightController.run();
+        
     }
 }
